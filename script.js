fc73b2e30f8b4db001cb77d5529b57ba100eb247
TIMGW-test <161503703+TIMGW-test@users.noreply.github.com>
2024-03-21 20:09:30 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -7,3 +7,29 @@ function myFunction() {
     x.className = "navbar";
   }
 }
+
+
+
+
+function loadStudy(id, imageSrc) {
+  // Store the image source in localStorage to be accessed in the template page
+  localStorage.setItem('imageSrc', imageSrc);
+  localStorage.setItem('studyId', id);
+  console.log("I am working");
+}
+
+function updateImage() {
+  // Retrieve the image source and ID from localStorage
+  var imageSrc = localStorage.getItem('imageSrc');
+  var studyId = localStorage.getItem('studyId');
+
+  // Check if imageSrc and studyId are not null
+  if (imageSrc !== null && studyId !== null) {
+      // Update the image source
+      document.getElementById("bibleStudyImgTemplate").src = imageSrc;
+      // Clear the localStorage to prevent the image from being loaded again
+      localStorage.removeItem('imageSrc');
+  }
+}
+
+window.onload = updateImage;
